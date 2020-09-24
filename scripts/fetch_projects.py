104b7d83a2e625be480eca039ce57ef2f63a0c8c
schnek <tolstikov.misha@gmail.com>
2020-09-24 21:02:42 +0300

    Update index.css

--- scripts/fetch_projects.py
+++ scripts/fetch_projects.py
@@ -152,6 +152,6 @@ with open(file_path, "w+") as f:
 print("LOG: Saved to", file_path)
 
 # Update the SVG
-with open("assets/framework_cover.jpg", "w+") as f:
+with open("assets/framework.svg", "w+") as f:
     f.write(network_svg)
-print("LOG: assets/framework_cover.jpg updated!")
+print("LOG: assets/framework.svg updated!")
