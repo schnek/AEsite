e80409fffde9f30dd5275fbce976ca837b29d0ae
schnek <tolstikov.misha@gmail.com>
2020-09-25 19:45:13 +0300

    v2

---
+++ index.md
@@ -0,0 +1,6 @@
+---
+layout: default
+---
+
+# Welcome to AscEmu 
+We moved our complete sitee to github pages.
--- js/discordapp.js
+++
@@ -1,7 +0,0 @@
-<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
-  new Crate({
-    server: '227808274488098819',
-    channel: '227808274488098819',
-    shard: 'https://discordapp.com/widget?id=227808274488098819&theme=light'
-  })
-</script>
